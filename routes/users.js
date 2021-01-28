71a96a320b1e21f0d1c16d441f4544d00e999745
Yaya <yayazdiallo@outlook.fr>
2021-01-28 17:47:25 +0100

    add tickets in users route

--- routes/users.js
+++ routes/users.js
@@ -3,37 +3,43 @@ var router = express.Router();
 const usersModel = require('../models/users');
 
 /* GET users listing. */
-router.post('/sign-up', async function(req,res,next){
-    /* GET USERS ALREADY OR NOT. */
+router.post('/sign-up', async function (req, res, next) {
+  /* GET USERS ALREADY OR NOT. */
   var searchUser = await usersModel.findOne({
-    email: req.body.emailFromFront
-  })
-  
-  if(!searchUser){
+    email: req.body.emailFromFront,
+  });
+
+  if (!searchUser) {
     var newUser = new usersModel({
       name: req.body.NameFromFront,
       firstName: req.body.FirstNameFromFront,
       email: req.body.emailFromFront,
       password: req.body.passwordFromFront,
-    })
-  
+    });
+
     var newUserSave = await newUser.save();
-  
+
     // rajouter la session pour chaque utilisateur//
     req.session.user = {
       email: req.body.emailFromFront,
       id: newUserSave._id,
-    }
-  
-    console.log(req.session.user, "hellooooooooooooooo");
-  
-    res.redirect('/homepage')
+    };
+
+    console.log(req.session.user, 'hellooooooooooooooo');
+
+    res.redirect('/homepage');
   } else {
-    res.redirect('/')
-  }});
+    res.redirect('/');
+  }
+});
 
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+/** Get My Tickets Page */
+router.get('/my-tickets', (req, res) => {
+  res.render('myTickets', { title: 'My tickets' });
 });
 
 /** Get My Last Trips Page */

743c9a8acb4540e7642ba8314338ef95333e6354
Yaya <yayazdiallo@outlook.fr>
2021-01-28 16:18:56 +0100

    lastTrips page

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+/** Get My Last Trips Page */
+router.get('/last-trips', (req, res) => {
+  res.render('lastTrips', { title: 'My last trips' });
 });
 
 module.exports = router;

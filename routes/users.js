34993810c5948224b9c2aef61305b0f282c89378
Gwendoline Pedurand <gwendoline.pedurand@gmail.com>
2021-01-28 16:31:12 +0100

    création de la users.js finalisation de la route signup

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+const usersModel = require('../models/users');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/sign-up', async function(req,res,next){
+    /* GET USERS ALREADY OR NOT. */
+  var searchUser = await usersModel.findOne({
+    email: req.body.emailFromFront
+  })
+  
+  if(!searchUser){
+    var newUser = new usersModel({
+      name: req.body.NameFromFront,
+      firstName: req.body.FirstNameFromFront,
+      email: req.body.emailFromFront,
+      password: req.body.passwordFromFront,
+    })
+  
+    var newUserSave = await newUser.save();
+  
+    // rajouter la session pour chaque utilisateur//
+    req.session.user = {
+      email: req.body.emailFromFront,
+      id: newUserSave._id,
+    }
+  
+    console.log(req.session.user, "hellooooooooooooooo");
+  
+    res.redirect('/homepage')
+  } else {
+    res.redirect('/')
+  }
+
 });
 
 module.exports = router;
